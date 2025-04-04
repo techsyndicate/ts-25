f61cfd9eddc1ad9c696d57d9276a93f9e0f31304
bhavitgrover <bhavit.grover@outlook.com>
2025-04-04 23:12:31 +0530

    perf: replaced window.location.href with next native router module

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,12 @@
 "use client";
 import React from "react";
+import { useRouter } from "next/navigation";
 
 function Navbar() {
+  const router = useRouter();
+
   return (
-    <div className="min-w-[100vw] my-5 flex flex-row items-center justify-center gap-2">
+    <div className="my-5 flex flex-row items-center justify-center gap-2">
       <div
         onClick={() => (window.location.href = "/")}
         className="border-2 border-[#363636] flex flex-row items-center justify-center px-20 py-2.5 rounded-lg gap-2 cursor-pointer"
@@ -12,11 +15,39 @@ function Navbar() {
         <h1>Tech Syndicate</h1>
       </div>
       <div className="bg-[#191919] py-3 px-20 flex flex-row items-center justify-center rounded-lg">
-        <div className="flex flex-row items-center justify-center gap-6">
-          <a href="/learn">Learn</a>
-          <a href="/about">About</a>
-          <a href="/team">Team</a>
-          <a href="/events">Events</a>
+        <div className="flex flex-row items-center justify-center gap-7">
+          <p
+            className="cursor-pointer"
+            onClick={() => {
+              router.push("/learn");
+            }}
+          >
+            Learn
+          </p>
+          <p
+            className="cursor-pointer"
+            onClick={() => {
+              router.push("/about");
+            }}
+          >
+            About
+          </p>
+          <p
+            className="cursor-pointer"
+            onClick={() => {
+              router.push("/team");
+            }}
+          >
+            Team
+          </p>
+          <p
+            className="cursor-pointer"
+            onClick={() => {
+              router.push("/events");
+            }}
+          >
+            Events
+          </p>
           {/* <a href="/intech">inTech</a> */}
         </div>
       </div>
