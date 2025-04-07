43f614b79e2fd05fa020c50912fad96292ac4547
Ishit Rastogi <ishitrastogi@gmail.com>
2025-04-07 19:17:50 +0530

    chore: prettier

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -11,14 +11,17 @@ function Team() {
     <div className="overflow-y-auto no-scrollbar h-screen w-screen">
       <div className="flex items-center justify-center flex-col mb-[5vh]">
         <Navbar />
-        <h2 className="text-[10vw] mt-[10vw] sm:mt-[4vw] sm:text-[3vw]">THE TEAM</h2>
+        <h2 className="text-[10vw] mt-[10vw] sm:mt-[4vw] sm:text-[3vw]">
+          THE TEAM
+        </h2>
         <button
           onClick={() => {
             router.push("/alumni");
           }}
           className="sm:text-[#777] text-[#fff] flex items-center sm:gap-[0.4vw] sm:text-[1.1vw] sm:cursor-pointer text-[5vw] gap-[2vw]"
         >
-          ALUMNI <img src="/rightArrow.png" className="sm:h-[0.9vw] h-[4vw]"></img>
+          ALUMNI{" "}
+          <img src="/rightArrow.png" className="sm:h-[0.9vw] h-[4vw]"></img>
         </button>
         <div className="sm:w-[80vw] flex sm:flex-row flex-col items-center justify-center sm:flex-wrap mt-[5vh] mb-[8vh] gap-[3vw] sm:wrap-anywhere">
           {members.map((member, index) => (
--- components/AlumniCardGrid.tsx
+++ components/AlumniCardGrid.tsx
@@ -32,18 +32,25 @@ const AlumniCardGrid: React.FC<AlumniGridProps> = ({ batch, alumnus = [] }) => {
   return (
     <>
       <div className="w-[80vw] flex flex-col items-center justify-center flex-wrap mt-[5vh] gap-[3vw] wrap-anywhere">
-        <h1 className="sm:text-[3vw] text-[8vw] text-[#16e16e] self-start">{batch}</h1>
+        <h1 className="sm:text-[3vw] text-[8vw] text-[#16e16e] self-start">
+          {batch}
+        </h1>
 
         <div className="sm:w-[80vw] flex sm:flex-row flex-col items-center justify-center sm:flex-wrap mb-[8vh] gap-[3vw] sm:wrap-anywhere">
           {alumnus.map((member, index) => {
             return (
-              <div className="sm:w-[20vw] sm:h-[30vw] w-[70vw] h-[100vw] flex flex-col items-center mt-[4vh] bg-[#191919] sm:rounded-[1vw] rounded-[4vw] group relative " key={index}>
+              <div
+                className="sm:w-[20vw] sm:h-[30vw] w-[70vw] h-[100vw] flex flex-col items-center mt-[4vh] bg-[#191919] sm:rounded-[1vw] rounded-[4vw] group relative "
+                key={index}
+              >
                 <img
                   src={member.pfp}
                   alt={member.name}
                   className="sm:w-[25vw] w-[70vw] aspect-square object-cover sm:p-[2vw] p-[4vw] sm:rounded-[2.6vw] rounded-[7vw]"
                 />
-                <h2 className="text-[#fff] sm:text-[2vw] text-[8vw]">{member.name}</h2>
+                <h2 className="text-[#fff] sm:text-[2vw] text-[8vw]">
+                  {member.name}
+                </h2>
                 <h4 className="sm:text-[#8c8c8c] sm:text-[1.4vw] text-[5vw] sm:group-hover:text-[#16e16e] text-[#16e16e] transition-all duration-300 mt-[1vw]">
                   {member.exrole}
                 </h4>
