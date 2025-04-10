3c21c5b8b137b6fdcc2d3b84547b598ede51d06b
bhavitgrover <bhavit.grover@outlook.com>
2025-04-10 23:18:25 +0530

    fix: fixed homepage scaling and white background on light theme

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,12 @@
 import React, { useEffect, useRef } from "react";
 import Navbar from "@/components/Navbar";
 import localFont from "next/font/local";
+import { Outfit } from "next/font/google";
+
+const outfitFont = Outfit({
+  variable: "--font-outfit",
+  subsets: ["latin"],
+});
 
 const headingFont = localFont({
   src: "../public/headingFont.otf",
@@ -60,75 +66,79 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="w-[100%] h-[100vh] overflow-hidden">
-      <Navbar />
-      <div className="relative">
-        <h1
-          className={`${headingFont.className} mt-[-5vh] text-[14vw] text-center`}
-          style={{
-            backgroundImage:
-              "linear-gradient(179deg, #FFF 0.45%, #5A5A5A 126.49%)",
-            backgroundClip: "text",
-            WebkitBackgroundClip: "text",
-            WebkitTextFillColor: "transparent",
-          }}
-        >
-          <span
+    <body
+      className={`w-[100vw] h-[100vh] overflow-hidden ${outfitFont.variable} ${outfitFont.className} antialiased m-0`}
+    >
+      <div>
+        <Navbar />
+        <div className="relative">
+          <h1
+            className={`${headingFont.className} mt-[-5vh] text-[14vw] text-center`}
             style={{
               backgroundImage:
-                "linear-gradient(179deg, #16E16E 0.45%, #084422 126.49%)",
+                "linear-gradient(179deg, #FFF 0.45%, #5A5A5A 126.49%)",
               backgroundClip: "text",
               WebkitBackgroundClip: "text",
               WebkitTextFillColor: "transparent",
             }}
           >
-            TECH
-          </span>{" "}
-          SYNDICATE
-        </h1>
-        <img
-          src="./home/cinema.svg"
-          className="absolute top-[-6vh] left-[19vw] scale-[0.7] parallax"
-          alt=""
-        />
-        <img
-          src="./home/o1.svg"
-          className="absolute top-[-6vh] right-[23vw] scale-[0.7] parallax"
-          alt=""
-        />
-        <img
-          src="./home/camera.svg"
-          className="absolute bottom-[-8vh] left-[25vw] z-[-10] scale-[0.7] parallax parallax-slow"
-          alt=""
-        />
-        <div className="absolute bottom-[7vh] right-[15.8vw] flex flex-row items-center justify-center gap-[1vw]">
-          <img src="./home/insta.svg" />
-          <img src="./home/gh.svg" alt="" />
-          <img src="./home/behance.svg" alt="" />
-          <img src="./home/facebook.svg" alt="" />
+            <span
+              style={{
+                backgroundImage:
+                  "linear-gradient(179deg, #16E16E 0.45%, #084422 126.49%)",
+                backgroundClip: "text",
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+              }}
+            >
+              TECH
+            </span>{" "}
+            SYNDICATE
+          </h1>
+          <img
+            src="./home/cinema.svg"
+            className="absolute top-[-4vh] left-[16vw] w-[13vw] parallax"
+            alt=""
+          />
+          <img
+            src="./home/o1.svg"
+            className="absolute top-[-1vh] w-[13vw] right-[22vw] parallax"
+            alt=""
+          />
+          <img
+            src="./home/camera.svg"
+            className="absolute bottom-[-2vh] left-[26vw] z-[-10] w-[15vw] parallax parallax-slow"
+            alt=""
+          />
+          <div className="absolute bottom-[7vh] right-[15.8vw] flex flex-row items-center justify-center gap-[1vw]">
+            <img className="w-[1.25vw]" src="./home/insta.svg" />
+            <img className="w-[1.25vw]" src="./home/gh.svg" alt="" />
+            <img className="w-[1.25vw]" src="./home/behance.svg" alt="" />
+            <img className="w-[1.25vw]" src="./home/facebook.svg" alt="" />
+          </div>
         </div>
+        <img
+          src="./home/camera2.svg"
+          className="absolute bottom-0 left-[9vw] w-[30vw] parallax parallax-fast"
+          alt=""
+        />
+        <img
+          src="./home/cube.svg"
+          className="absolute top-[35vh] right-[27vw] w-[14.4vw] parallax parallax-slow"
+          alt=""
+        />
+        <img
+          src="./home/cam3.svg"
+          className="absolute bottom-0 right-[15vw] w-[19.3vw] parallax parallax-fast"
+          alt=""
+        />
+        <img
+          src="./home/man.svg"
+          className="absolute bottom-[-2vh] left-[33vw] w-[35vw]"
+          alt=""
+        />
+        <div className="w-[20vw] absolute left-[39.5vw] z-[-100] aspect-square bg-[#16e16e] blur-[200px]" />
       </div>
-      <img
-        src="./home/camera2.svg"
-        className="absolute bottom-0 left-[3vw] scale-[0.9] parallax parallax-fast"
-        alt=""
-      />
-      <img
-        src="./home/cube.svg"
-        className="absolute top-[35vh] right-[27vw] scale-[0.7] parallax parallax-slow"
-        alt=""
-      />
-      <img
-        src="./home/cam3.svg"
-        className="absolute bottom-0 right-[10vw] parallax parallax-fast"
-        alt=""
-      />
-      <img
-        src="./home/man.svg"
-        className="absolute bottom-[-2vh] left-[32vw] scale-[0.95]"
-        alt=""
-      />
-      <div className="w-[20vw] absolute left-[39.5vw] z-[-100] aspect-square bg-[#16e16e] blur-[200px]" />
-    </div>
+    </body>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,12 +9,12 @@ function Navbar() {
     <div className="my-[1.8vw] flex flex-row items-center justify-center gap-[0.7vw]">
       <div
         onClick={() => (window.location.href = "/")}
-        className="border-2 border-[#363636] flex flex-row items-center justify-center px-[6vw] py-[0.68vw] rounded-lg gap-[0.5vw] cursor-pointer"
+        className="border-2 border-[rgba(255,255,255,0.17)] flex flex-row items-center justify-center px-[5vw] py-[0.68vw] rounded-lg gap-[0.5vw] cursor-pointer"
       >
         <img src="./tsLogo.svg" alt="TS Logo" className="size-[1.5vw]" />
         <h1>Tech Syndicate</h1>
       </div>
-      <div className="bg-[#191919] py-[1.8vh] px-[5vw] flex flex-row items-center justify-center rounded-lg">
+      <div className="bg-[rgba(51,51,51,0.3)] bg- py-[1.8vh] px-[5vw] flex flex-row items-center justify-center rounded-lg">
         <div className="flex flex-row items-center justify-center gap-[2vw]">
           <p
             className="cursor-pointer"
